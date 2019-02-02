a0394969557f2a7622e57b5bd8063ff55330b2f3
ozturk <ozturktx@gmail.com>
2019-02-01 19:35:08 -0600

    first push

---
+++ src/test/java/Tests/LoginTests.java
@@ -0,0 +1,36 @@
+package Tests;
+
+import com.sun.xml.internal.ws.server.DefaultResourceInjector;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void test1()
+    {
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+        driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+        Assert.assertEquals(driver.getTitle(),"Web Orders");
+    }
+
+}
